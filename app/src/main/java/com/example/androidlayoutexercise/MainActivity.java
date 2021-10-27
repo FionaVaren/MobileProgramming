fca58376bb43bb7280f08cfdfe964b7c9f45e030
FionaVaren <fionatendio@gmail.com>
2021-10-27 10:17:38 +0700

    upload tugas layout exercise

---
+++ app/src/main/java/com/example/androidlayoutexercise/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidlayoutexercise;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
